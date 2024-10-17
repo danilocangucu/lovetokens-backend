d8003b3cbfe892d9b24f0a5e4a9dbb946eb06dda
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-17 14:29:51 +0200

    draft: health check

--- src/routers/loveTokenRouter.ts
+++ src/routers/loveTokenRouter.ts
@@ -4,5 +4,9 @@ import { fetchFeaturedLoveTokens } from "../controllers/loveTokenController";
 const router = Router();
 
 router.get("/featured-love-tokens", fetchFeaturedLoveTokens);
+// TODO implement proper health check
+router.get("/health", (_, res) => {
+  res.send("OK");
+});
 
 export default router;
